726485226b892114df17bd3410b0e4fe2275917d
higi <calusari22@gmail.com>
2018-06-01 15:04:25 +0200

    infos

---
+++ _shell_scripts/C_V10_ETH/startgabi.sh
@@ -0,0 +1,4 @@
+
+export GPU_MAX_ALLOC_PERCENT=100
+
+./ethdcrminer64 -epool eu1.ethermine.org:4444 -ewal 0x3FFDE66A057886DC5acE89448959ced8cFea4E74.gabi1 -epsw x -mode 1 -tt 65,65,65,65,65,65,65,65,65,65,65,65 -fanmin 35,35,35,35,35,35,35,35,35,35,35,35 -tstop 85,85,85,85,85,85,85,85,85,85,85,85
