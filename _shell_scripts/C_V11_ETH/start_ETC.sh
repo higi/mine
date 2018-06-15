d6576844aa762f29c7b4c24d1c495f0eaaa68c5d
higi <calusari22@gmail.com>
2018-06-15 14:06:11 +0200

    - new stuff clamore v11 dcri options

---
+++ _shell_scripts/C_V11_ETH/start_ETC.sh
@@ -0,0 +1,3 @@
+export GPU_MAX_ALLOC_PERCENT=100
+
+./ethdcrminer64 -epool eu1-etc.ethermine.org:4444 -ewal 0x4b9074952eac65763c438d3ff9ea65bbcf7c59af.10rx580home2 -epsw x -mode 1 -tt 65,65,65,65,65,65,65,65,65,65,65,65 -fanmin 35,35,35,35,35,35,35,35,35,35,35,35 -tstop 85,85,85,85,85,85,85,85,85,85,85,85
