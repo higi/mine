9bd6d81c5bc7f4317da4aa738fd9b303959de288
higi <calusari22@gmail.com>
2018-11-11 02:48:17 +0100

    - cleanup + new stuff

---
+++ SET_ALL_FAN_35.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+/opt/rocm/bin/rocm-smi -d 1 --setfan 93
+/opt/rocm/bin/rocm-smi -d 2 --setfan 93
+/opt/rocm/bin/rocm-smi -d 3 --setfan 93
+/opt/rocm/bin/rocm-smi -d 4 --setfan 93
+/opt/rocm/bin/rocm-smi -d 5 --setfan 93
+/opt/rocm/bin/rocm-smi -d 6 --setfan 93
+/opt/rocm/bin/rocm-smi -d 7 --setfan 93
+/opt/rocm/bin/rocm-smi -d 8 --setfan 93
---
+++ getGPU_SUMMARY_INFO.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/opt/rocm/bin/rocm-smi
+
